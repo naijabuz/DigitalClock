b85496d62610662ed4039e97e66b48a5a62862d6
Falade Yusuf <52901501+naijabuz@users.noreply.github.com>
2023-06-19 17:54:12 +0100

    removed the comments

--- index.js
+++ index.js
@@ -1,85 +1,47 @@
-// // A function for the digital clock dynamic
+// A function for the digital clock dynamic
 
-// const digitalClock = function digitalClock() {
+const digitalClock = function digitalClock() {
 
-//     const hours = document.getElementById("hours"); // variable that stores the id from html element
-//     const minutes = document.getElementById("minutes"); // ditto
-//     const seconds = document.getElementById("seconds"); // ditto
-//     const daytype = document.getElementById("ampm"); // ditto
+    const hours = document.getElementById("hours"); // variable that stores the id from html element
+    const minutes = document.getElementById("minutes"); // ditto
+    const seconds = document.getElementById("seconds"); // ditto
+    const daytype = document.getElementById("ampm"); // ditto
 
-//     let hr = new Date().getHours(); // hour value (number) is stored in variable
-//     let mins = new Date().getMinutes(); // minute value (number) is stored in variable
-//     let secs = new Date().getSeconds(); // seconds value (number) is stored in variable
-//     let ampm = 'AM'; // new value for the daytype
+    let hr = new Date().getHours(); // hour value (number) is stored in variable
+    let mins = new Date().getMinutes(); // minute value (number) is stored in variable
+    let secs = new Date().getSeconds(); // seconds value (number) is stored in variable
+    let ampm = 'AM'; // new value for the daytype
 
-// // If statement to change the clock setting from 24hrs to 12hrs and to also auto update between AM and PM
-//     if (hr > 12) {
-//         hr = hr - 12;
-//         ampm = "PM";
-//     } else if (hr = 12) {
-//         ampm = "PM"
-//     };
-
-// // ternary if statement to add 0 in front of the time values when they are between 0 to 9
-//     hr = hr < 10
-//     ? "0" + hr
-//     : hr
-
-//     mins = mins < 10 ? "0" + mins : mins
-//     secs = secs < 10 ? "0" + secs : secs
-   
-// // Replacing the default hard coded values in the HTML pages with the javascript values
-//     hours.innerText = hr;
-//     minutes.innerText = mins;
-//     seconds.innerText = secs;
-//     daytype.innerText = ampm;
-   
-// // function for restarting the clock every 1 seconds so you dont have to refresh all the time.
-//     setTimeout(()=>{
-//         digitalClock()
-//     }, 1000)
-
-// }
-
-// // Starting the function
-// digitalClock();
-
-const digitalClock = () => {
-
-    const hours = document.getElementById("hours");
-    const minutes = document.getElementById("minutes");
-    const seconds = document.getElementById("seconds");
-    const daytype = document.getElementById("ampm");
-
-    let hr = new Date().getHours();
-    let mins = new Date().getMinutes();
-    let secs = new Date().getSeconds();
-    let ampm = "AM";
-
+// If statement to change the clock setting from 24hrs to 12hrs and to also auto update between AM and PM
     if (hr > 12) {
-        ampm = "PM"
-        hr = hr - 12
+        hr = hr - 12;
+        ampm = "PM";
     } else if (hr = 12) {
         ampm = "PM"
     };
 
-    hr = hr < 10 ? "0" + hr : hr;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10
-    ? "0" + secs
-    : secs;
+// ternary if statement to add 0 in front of the time values when they are between 0 to 9
+    hr = hr < 10
+    ? "0" + hr
+    : hr
 
-    hours.innerText = hr; 
+    mins = mins < 10 ? "0" + mins : mins
+    secs = secs < 10 ? "0" + secs : secs
+   
+// Replacing the default hard coded values in the HTML pages with the javascript values
+    hours.innerText = hr;
     minutes.innerText = mins;
     seconds.innerText = secs;
     daytype.innerText = ampm;
-
+   
+// function for restarting the clock every 1 seconds so you dont have to refresh all the time.
     setTimeout(()=>{
         digitalClock()
-    },1000);
+    }, 1000)
 
-};
+}
 
+// Starting the function
 digitalClock();
 
 
